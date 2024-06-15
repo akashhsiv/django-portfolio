63f989e2dc39edeaab042447c7e144542df267b7
akashhsiv <akashhsiv@gmail.com>
2024-06-15 14:48:53 +0530

    updated

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -124,13 +124,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'staticfiles/'
+STATIC_URL = 'static'
 MEDIA_URL='/images/'
+MEDIA_ROOT= BASE_DIR/'media'
 
 STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'staticfiles')
+    os.path.join(BASE_DIR, 'static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
