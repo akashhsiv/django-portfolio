74d3f33176568ccf46d41a474d9b1b5a0435bec0
akashhsiv <akashhsiv@gmail.com>
2024-06-15 15:01:57 +0530

    updated

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -128,9 +128,8 @@ STATIC_URL = 'static'
 MEDIA_URL='/images/'
 MEDIA_ROOT= BASE_DIR/'media'
 
-STATICFILES_DIRS =[
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS =    os.path.join(BASE_DIR, 'static')
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
