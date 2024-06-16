a0cf79325932607a9adbc6a926d3b2f0ec76aca8
akashhsiv <akashhsiv@gmail.com>
2024-06-16 07:54:33 +0530

    updated

--- django_portfolio/wsgi.py
+++ django_portfolio/wsgi.py
@@ -11,7 +11,6 @@ import os
 import sys
 
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_portfolio.settings')
 
 if os.environ.get('ENV') == 'production':
